d512fee281b58fdce17db008a9bb5338cd7b9102
kotaro_k <kotaro.k13@gmail.com>
2018-11-23 21:46:04 +0900

    ex-1 auto-routingによるNuxtライクなルーティング処理

---
+++ handsonvuecliv3/src/pages/foo.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <h1>Foo page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- handsonvuecliv3/src/pages/index.vue
+++ handsonvuecliv3/src/pages/index.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    {{count}}
+    <img src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -9,14 +8,8 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
-  computed: {
-    count () {
-      return this.$store.state.count
-    }
-  },
   components: {
     HelloWorld
   }
