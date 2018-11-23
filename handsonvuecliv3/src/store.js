467eb540820d1a7bc8c413fa8a8b6cd7436fc8db
DESKTOP-Q7A8B81\kidok <kotaro.k13@gmail.com>
2018-11-23 20:43:38 +0900

    5.Vuex ストアへのカウンタ機能の実装

--- handsonvuecliv3/src/store.js
+++ handsonvuecliv3/src/store.js
@@ -3,12 +3,17 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export const state = {
-
-}
+export const state = () => ({
+  count: 0
+})
 
 export const mutations = {
-
+  increment (state) {
+    state.count++
+  },
+  decrement (state) {
+    state.count--
+  }
 }
 
 export const actions = {
--- handsonvuecliv3/tests/unit/store.spec.js
+++ handsonvuecliv3/tests/unit/store.spec.js
@@ -3,21 +3,21 @@ import { createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
 
 const localVue = createLocalVue()
-localVue.user(Vuex)
+localVue.use(Vuex)
 
 describe('store.js', () => {
   describe('mutations', () => {
     test('increment', () => {
       const store = new Vuex.Store({ ...baseStore })
-      expect((store.state.count)).toBe(0)
+      expect(store.state.count).toBe(0)
       store.commit('increment')
-      expect((store.state.count)).toBe(1)
+      expect(store.state.count).toBe(1)
     })
     test('decrement', () => {
       const store = new Vuex.Store({ ...baseStore })
-      expect((store.state.count)).toBe(0)
+      expect(store.state.count).toBe(0)
       store.commit('decrement')
-      expect((store.state.count)).toBe(-1)
+      expect(store.state.count).toBe(-1)
     })
   })
 })
